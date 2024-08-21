d36c65fb9f44b4784ff50da8638e0617d86d3d39
vozzmak <paulbertosh@gmail.com>
2024-08-21 23:59:30 +0300

    init commit

---
+++ src/main/java/com/telegram/coinmarketbot/CoinMarketBotApplication.java
@@ -0,0 +1,13 @@
+package com.telegram.coinmarketbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinMarketBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinMarketBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telegram/coinmarketbot/CoinMarketBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegram.coinmarketbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinMarketBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
